616b843e66583794059822e48854ba16a74b9424
Ruth <ruro122020@gmail.com>
2024-06-04 18:59:55 -0400

    created pets table

--- models/models.py
+++ models/models.py
@@ -1,8 +1,8 @@
 #Association tables need to be imported before the 2 table that association table is using 
 from .speciesclassification import SpeciesClassification
 from .symptomsclassifications import SymptomClassification
-from .usersspecies import UsersSpecies
 from .species import Species
 from .classification import Classification
 from .symptom import Symptom
 from .user import User
+from .pets import Pet
