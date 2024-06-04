ace5cffe31abf8c4bd9ddff45af1e2fc70522c08
Ruth <ruro122020@gmail.com>
2024-06-04 16:21:32 -0400

    create SpeciesResource complete

--- routes/species/species_resource.py
+++ routes/species/species_resource.py
@@ -4,10 +4,11 @@ from config import app, db, api
 from models.models import Species
 from marshmallow_schemas.species import species_schema_many
 
+#SpeciesResource was used to avoid name conflict with Species model
 class SpeciesResource(Resource):
   def get(self):
     species = Species.query.all()
     return species_schema_many.dump(species)
 
 
-api.add_resource(SpeciesResource, '/species')
+api.add_resource(SpeciesResource, '/species', endpoint='species')
