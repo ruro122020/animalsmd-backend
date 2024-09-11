e048b6ecb81e11f90aad279cc9bb6bca55e96c18
ruth <ruro122020@gmail.com>
2024-09-10 22:36:27 -0400

    seed illnessproduct table

--- seed/illnessproduct/illness_product.py
+++ seed/illnessproduct/illness_product.py
@@ -1,30 +1,16 @@
 from config import app
+from models import product
 from .illnesses_products_data import illness_product_data
-from models.models import IllnessMedication
+from models.models import IllnessProduct, Illness, Product
 
 
 def seed_illness_product_table():
    with app.app_context():
-      illness_medication_table_data = IllnessMedication.query.all()
-      print('illness medications data',illness_medication_table_data[0].illness.name)
-      print('illness medications data',illness_medication_table_data[1].illness.name)
-      print('illness medications data',illness_medication_table_data[2].illness.name)
-      print('illness medications data',illness_medication_table_data[3].illness.name)
-
-
-      #use the illness name
-      #if illness name in illness_product_data matches the illness name in illness_medication_table 
-        #query the illness table by using the illness name 
-        #grab the illness medication id
-        #query the medication by id in the database
-        #query the products table by using the medication name
-
-        #We want to locate the product instance from the product table by using the product that 
-        # matches the medication
-        #We want to locate the illness instance from the illness table by using the illness name 
-        # in illness_product_data
-
-      pass
+      for obj in illness_product_data:
+        illness = Illness.query.filter_by(name = obj['illness']).first()
+        for product_name in obj['products']:
+          product = Product.query.filter_by(name = product_name).first()
+          IllnessProduct.create_row(illness=illness, product=product)
    
 
 
