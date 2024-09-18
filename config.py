2273187bdc3a64d5d38b186c2a6f8d5352e6b920
ruth <ruro122020@gmail.com>
2024-09-18 17:53:53 -0400

    deploy in progress

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ import os
 supauser = os.getenv('SUPAUSER')
 supapassword =os.getenv('SUPAPASSWORD')
 supadbname =os.getenv('SUPADBNAME')
-supaport = float(os.getenv('SUPAPORT'))
+supaport = int(os.getenv('SUPAPORT'))
 
 # Instantiate app, set attributes
 app = Flask(__name__)
