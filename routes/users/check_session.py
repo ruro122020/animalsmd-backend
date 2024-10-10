347da2a20aff1cd324f49c231670a641f1e6f711
ruth <ruro122020@gmail.com>
2024-10-09 21:13:59 -0400

    changed response from check_session resource

--- routes/users/check_session.py
+++ routes/users/check_session.py
@@ -6,10 +6,20 @@ from marshmallow_schemas.users import user_schema
 
 class CheckSession(Resource):
   def get(self):
-    if session.get('user_id'):
-      user = User.query.filter(User.id == session.get('user_id')).first()
-      return user_schema.dump(user), 200
-    return {}, 401
+    
+    if not session.get('user_id'):
+      return {
+        "status":"failed",
+        "error":{"message": "User Not logged in"},
+        "code": 401
+      }
+    
+    user = User.query.filter(User.id == session.get('user_id')).first()
+    return {
+      "status": "success",
+      "data": user_schema.dump(user),
+      "code": 200
+    }
   
 
 api.add_resource(CheckSession, '/check_session')
