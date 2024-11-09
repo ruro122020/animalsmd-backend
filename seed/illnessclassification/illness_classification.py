8c536f9522f6ecffa8e4b80bbb3046d01dd5fed2
ruth <ruro122020@gmail.com>
2024-11-09 13:33:02 -0500

    populated illnesses classifications table

--- seed/illnessclassification/illness_classification.py
+++ seed/illnessclassification/illness_classification.py
@@ -5,18 +5,16 @@ from .illnesses_classifications_data import illness_classification_data
 
 def seed_illness_classification():
   with app.app_context():
-    for classification_name in illness_classification_data:
-      #query classification table for classification id 
+    for classification_name, illness_list in illness_classification_data.items():
       classification = Classification.query.filter_by(classification = classification_name).first()
-      
-    
-      for illness_name in illness_classification_data[classification_name]:
+
+      for illness_name in illness_list:
         illness = Illness.query.filter_by(name=illness_name).first()
-        #check if illness exist in illnessesclassifications table
+        #in illnessclassification table each illness should be assigned to a classification id number
+        #check if illness already exist in illnessesclassifications table 
         illness_exist = IllnessClassification.query.filter_by(illness_id = illness.id ).first()
         if not illness_exist:
-          #create a row in IllnessClassifications with the illness id and classification id
-          IllnessClassification.create_row(illness, classification)  
+          IllnessClassification.create_row(illness, classification)
 
 
 seed_illness_classification()  
