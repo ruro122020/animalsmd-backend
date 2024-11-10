195733ff13f32403f83e17afff65e59eb2a6e10b
ruth <ruro122020@gmail.com>
2024-11-10 14:06:22 -0500

    create get_full_records function complete

--- seed/illnessclassification/illness_classification.py
+++ seed/illnessclassification/illness_classification.py
@@ -2,6 +2,39 @@ from config import db, app
 from models.models import IllnessClassification, Illness, Classification
 from .illnesses_classifications_data import illness_classification_data
 
+#Steps
+#STEP 1 -- COMPLETE
+#we want to convert the data structure to be as follows:
+#{
+#   "reptile": ["scale rot"],
+#   "ave":['avian flu',"psittacosis"],
+#   "mammal": ["respiratory infection", "rabies", "canine distemper"]
+# }
+# ---create a function that will take in an list and convert it into the data structure above --- 
+
+#STEP 2 - COMPLETE
+#we need records of all the classifications, illnesses, and it's relationships that are in the 
+#database. 
+# ---create a function to returns a collection of classifications
+# ---create a function to return a collection of illnesses
+# ---create a function to return a collection of the relationship between illness and 
+#classifications in illnessclasssification table
+
+#STEP 3
+#we want check if that illness already exist in the illnessClassification table
+# --- create a function to return true if illness exist or return false if illness does
+#NOT exist
+
+#STEP 4
+#we want to create a record in the illnessclassifications table
+# #if the illness does NOT exist
+# ---create a function to create the record
+
+
+def get_full_records(model):
+  with app.app_context():
+    return model.query.all()
+
 #convert_data_to_dict function converts the list to the data structure below:
 #{
 #   "reptile": ["scale rot"],
@@ -33,29 +66,7 @@ def seed_illness_classification():
 
 # seed_illness_classification()  
 
-#Steps
-#we want to convert the data structure to be as follows:
-#{
-#   "reptile": ["scale rot"],
-#   "ave":['avian flu',"psittacosis"],
-#   "mammal": ["respiratory infection", "rabies", "canine distemper"]
-# }
-# ---create a function that will take in an list and convert it into the data structure above --- complete
 
-#we need records of all the classifications, illnesses, and it's relationships that are in the 
-#database. 
-# ---create a function to returns a collection of classifications
-# ---create a function to return a collection of illnesses
-# ---create a function to return a collection of the relationship between illness and 
-#classifications in illnessclasssification table
-
-#we want check if that illness already exist in the illnessClassification table
-# --- create a function to return true if illness exist or return false if illness does
-#NOT exist
-
-#we want to create a record in the illnessclassifications table
-# #if the illness does NOT exist
-# ---create a function to create the record
 
 
 
