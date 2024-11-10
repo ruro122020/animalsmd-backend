cbdc6a9863ecbec6f89bc6001040f84370e3261f
ruth <ruro122020@gmail.com>
2024-11-10 13:52:13 -0500

    create convert_data_to_dict function complete

--- seed/illnessclassification/illness_classification.py
+++ seed/illnessclassification/illness_classification.py
@@ -2,8 +2,22 @@ from config import db, app
 from models.models import IllnessClassification, Illness, Classification
 from .illnesses_classifications_data import illness_classification_data
 
+#convert_data_to_dict function converts the list to the data structure below:
+#{
+#   "reptile": ["scale rot"],
+#   "ave":['avian flu',"psittacosis"],
+#   "mammal": ["respiratory infection", "rabies", "canine distemper"]
+# }
+def convert_data_to_dict(list):
+  converted_obj = {}
+  for obj in list:
+      converted_obj[obj.get('classification')] = obj.get('illnesses')
+  return converted_obj
+
+
 
 def seed_illness_classification():
+ 
   with app.app_context():
     for classification_name, illness_list in illness_classification_data.items():
       classification = Classification.query.filter_by(classification = classification_name).first()
@@ -17,4 +31,31 @@ def seed_illness_classification():
           IllnessClassification.create_row(illness, classification)
 
 
-seed_illness_classification()  
+# seed_illness_classification()  
+
+#Steps
+#we want to convert the data structure to be as follows:
+#{
+#   "reptile": ["scale rot"],
+#   "ave":['avian flu',"psittacosis"],
+#   "mammal": ["respiratory infection", "rabies", "canine distemper"]
+# }
+# ---create a function that will take in an list and convert it into the data structure above --- complete
+
+#we need records of all the classifications, illnesses, and it's relationships that are in the 
+#database. 
+# ---create a function to returns a collection of classifications
+# ---create a function to return a collection of illnesses
+# ---create a function to return a collection of the relationship between illness and 
+#classifications in illnessclasssification table
+
+#we want check if that illness already exist in the illnessClassification table
+# --- create a function to return true if illness exist or return false if illness does
+#NOT exist
+
+#we want to create a record in the illnessclassifications table
+# #if the illness does NOT exist
+# ---create a function to create the record
+
+
+
--- seed/illnessclassification/illnesses_classifications_data.py
+++ seed/illnessclassification/illnesses_classifications_data.py
@@ -1,6 +1,22 @@
-illness_classification_data = {
-  "reptile": ["scale rot"],
-  "ave":['avian flu',"psittacosis"],
-  "mammal": ["respiratory infection", "rabies", "canine distemper"]
+# illness_classification_data = {
+#   "reptile": ["scale rot"],
+#   "ave":['avian flu',"psittacosis"],
+#   "mammal": ["respiratory infection", "rabies", "canine distemper"]
+# }
 
-}
+
+illness_classification_data = [
+  {
+    "classification": "reptile",
+    "illnesses": ["scale rot"]
+  },
+  {
+    "classification": "ave",
+    "illnesses":['avian flu',"psittacosis"]
+  },
+  {
+    "classification": "mammal",
+    "illnesses":["respiratory infection", "rabies", "canine distemper"]
+  }
+]
+
