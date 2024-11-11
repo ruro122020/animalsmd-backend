fa508c9ecd351529f7f687164a3f345b78488e3d
ruth <ruro122020@gmail.com>
2024-11-11 18:33:03 -0500

    populated illnessesmedications table complete

--- seed/illnessmedication/illness_medication.py
+++ seed/illnessmedication/illness_medication.py
@@ -1,22 +1,7 @@
 from config import app
+from models import illnessclassification
 from .illnesses_medications_data import illness_medication_data
 from models.models import IllnessMedication, Illness, Medication
-
-#STEPS
-#STEP 1 - complete
-# --- create a function to convert the data structure to something like the following:
-#{
-#  'respiratory infection': ['enrofloxacin (Baytril)', 'ceftazidime (Fortaz)']
-#}
-
-# Once data structure is converted, iterate through it
-#for illness, medications in converted_data:
-#for medication in medications:
-#check if medication exist in IllnessMedication
-# --- create a function to check if medication exist in illnessMedication table
-#if medication exist, do nothing
-#if medication doesn't exist then create the record
-
 
 def convert_data(list):
   new_data = {}
@@ -28,6 +13,7 @@ def retrieve_record(attribute, name, list):
   for instance in list:
     if getattr(instance, attribute) == name:
       return instance
+  return None
     
 
 def has_record(attribute, medication_id, list):
@@ -36,95 +22,29 @@ def has_record(attribute, medication_id, list):
       return True
   return False
 
+def retrieve_all_records(model):
+  return model.query.all()
+
+def create_records_in_db(illness, medications):
+  illness_medication_collection = retrieve_all_records(IllnessMedication)
+  medications_collection = retrieve_all_records(Medication)
+  illnesses_collection = retrieve_all_records(Illness)
+  for medication_name in medications:
+    medication_instance = retrieve_record('name', medication_name, medications_collection)
+    exist = has_record('medication_id', medication_instance.id, illness_medication_collection)
+    if not exist:
+      illness_instance = retrieve_record('name', illness, illnesses_collection)
+      IllnessMedication.create_row(illness_instance, medication_instance)
+
+
 
 def seed_illness_medications_table():
   with app.app_context():
-    #query the database to get all the record in illness_medication
-    illness_medication_collection = IllnessMedication.query.all()
-    medications_collection = Medication.query.all()
     new_illness_medication_data = convert_data(illness_medication_data)
-
     for illness, medications in new_illness_medication_data.items():
-      for medication_name in medications:
-        #iterate through medications_collection to find the medication instance so we have access to medication id
-        #--- create a function to return the medication instance
-        # iterate through illness_medication_collection to check if medication exist 
-        #---create a function to check if medication exist in the illness_medication_collection
-        #create record if medication id is not found in illness_medication_collection
-        # - to create the record we need the illness and medication instance
-        pass
+      create_records_in_db(illness, medications)
+
 
 
         
 seed_illness_medications_table()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # with app.app_context():
-  #   for illness_name, medications in illness_medication_data.items():
-  #     #get illness from illnesses table
-  #     illness = Illness.query.filter_by(name=illness_name).first()
-  #     #check if illness ID is in illnessmedication table
-  #     illness_exist = IllnessMedication.query.filter_by(illness_id = illness.id).first()
-  #     if not illness_exist:
-  #       for medication_name in medications:
-  #         #get medication from medications table
-  #         medication = Medication.query.filter_by(name=medication_name).first()
-  #         #create row
-  #         IllnessMedication.create_row(illness, medication)
--- seed/illnessmedication/illnesses_medications_data.py
+++ seed/illnessmedication/illnesses_medications_data.py
@@ -1,37 +1,9 @@
-# illness_medication_data = {
-#   "respiratory infection":[
-#     "amoxicillin", 
-#     "doxycycline", 
-#     "enrofloxacin (Baytril)"
-#     ],
-#   "scale rot":[
-#     'enrofloxacin (Baytril)',
-#     'ceftazidime (Fortaz)'
-#     ],
-#   "avian flu":[],
-#   "rabies": [
-#   "rabies_vaccine",
-#   "rabies_immunoglobulin"
-#     ],
-#   "canine_distemper": [
-#     "doxycycline",
-#     "amoxicillin",
-#     "phenobarbital",
-#     "potassium_bromide"
-#   ],
-#   "psittacosis": [
-#     "doxycycline"
-#   ]
-# }
-
-
-
 illness_medication_data = [
-    {'illness': 'respiratory infection', 'medications': ['amoxicillin', 'doxycycline', 'enrofloxacin (Baytril)']},
-    {'illness': 'scale rot', 'medications': ['enrofloxacin (Baytril)', 'ceftazidime (Fortaz)']},
+    {'illness': 'respiratory infection', 'medications': ['amoxicillin', 'doxycycline', 'enrofloxacin (baytril)']},
+    {'illness': 'scale rot', 'medications': ['enrofloxacin (baytril)', 'ceftazidime (fortaz)']},
     {'illness': 'avian flu', 'medications': []},
-    {'illness': 'rabies', 'medications': ['rabies_vaccine', 'rabies_immunoglobulin']},
-    {'illness': 'canine_distemper', 'medications': ['doxycycline', 'amoxicillin', 'phenobarbital', 'potassium_bromide']},
+    {'illness': 'rabies', 'medications': ['rabies vaccine', 'rabies immunoglobulin']},
+    {'illness': 'canine distemper', 'medications': ['doxycycline', 'amoxicillin', 'phenobarbital', 'potassium bromide']},
     {'illness': 'psittacosis', 'medications': ['doxycycline']}
 ]
 
