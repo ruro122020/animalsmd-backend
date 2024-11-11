2335d2a99ac702509a76bd4a0372afab37869332
ruth <ruro122020@gmail.com>
2024-11-11 13:06:19 -0500

    create convert_data function complete

--- seed/illnessmedication/illness_medication.py
+++ seed/illnessmedication/illness_medication.py
@@ -2,19 +2,101 @@ from config import app
 from .illnesses_medications_data import illness_medication_data
 from models.models import IllnessMedication, Illness, Medication
 
+#STEPS
+#STEP 1 - complete
+# --- create a function to convert the data structure to something like the following:
+#{
+#  'respiratory infection': ['enrofloxacin (Baytril)', 'ceftazidime (Fortaz)']
+#}
+
+#
+# Once data structure is converted, iterate through it
+#for illness, medications in converted_data:
+#for medication in medications:
+#check if medication exist in IllnessMedication
+# --- create a function to check if medication exist in illnessMedication table
+#if medication exist, do nothing
+#if medication doesn't exist then create the record
+
+
+def convert_data(list):
+  new_data = {}
+  for dict in list:
+    new_data[dict.get('illness')] = dict.get('medications')
+  return new_data
+
 def seed_illness_medications_table():
-  
-  with app.app_context():
-    for illness_name, medications in illness_medication_data.items():
-      #get illness from illnesses table
-      illness = Illness.query.filter_by(name=illness_name).first()
-      #check if illness ID is in illnessmedication table
-      illness_exist = IllnessMedication.query.filter_by(illness_id = illness.id).first()
-      if not illness_exist:
-        for medication_name in medications:
-          #get medication from medications table
-          medication = Medication.query.filter_by(name=medication_name).first()
-          #create row
-          IllnessMedication.create_row(illness, medication)
+  pass
         
 seed_illness_medications_table()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # with app.app_context():
+  #   for illness_name, medications in illness_medication_data.items():
+  #     #get illness from illnesses table
+  #     illness = Illness.query.filter_by(name=illness_name).first()
+  #     #check if illness ID is in illnessmedication table
+  #     illness_exist = IllnessMedication.query.filter_by(illness_id = illness.id).first()
+  #     if not illness_exist:
+  #       for medication_name in medications:
+  #         #get medication from medications table
+  #         medication = Medication.query.filter_by(name=medication_name).first()
+  #         #create row
+  #         IllnessMedication.create_row(illness, medication)
--- seed/illnessmedication/illnesses_medications_data.py
+++ seed/illnessmedication/illnesses_medications_data.py
@@ -1,25 +1,37 @@
-illness_medication_data = {
-  "respiratory infection":[
-    "amoxicillin", 
-    "doxycycline", 
-    "enrofloxacin (Baytril)"
-    ],
-  "scale rot":[
-    'enrofloxacin (Baytril)',
-    'ceftazidime (Fortaz)'
-    ],
-  "avian flu":[],
-  "rabies": [
-  "rabies_vaccine",
-  "rabies_immunoglobulin"
-    ],
-  "canine_distemper": [
-    "doxycycline",
-    "amoxicillin",
-    "phenobarbital",
-    "potassium_bromide"
-  ],
-  "psittacosis": [
-    "doxycycline"
-  ]
-}
+# illness_medication_data = {
+#   "respiratory infection":[
+#     "amoxicillin", 
+#     "doxycycline", 
+#     "enrofloxacin (Baytril)"
+#     ],
+#   "scale rot":[
+#     'enrofloxacin (Baytril)',
+#     'ceftazidime (Fortaz)'
+#     ],
+#   "avian flu":[],
+#   "rabies": [
+#   "rabies_vaccine",
+#   "rabies_immunoglobulin"
+#     ],
+#   "canine_distemper": [
+#     "doxycycline",
+#     "amoxicillin",
+#     "phenobarbital",
+#     "potassium_bromide"
+#   ],
+#   "psittacosis": [
+#     "doxycycline"
+#   ]
+# }
+
+
+
+illness_medication_data = [
+    {'illness': 'respiratory infection', 'medications': ['amoxicillin', 'doxycycline', 'enrofloxacin (Baytril)']},
+    {'illness': 'scale rot', 'medications': ['enrofloxacin (Baytril)', 'ceftazidime (Fortaz)']},
+    {'illness': 'avian flu', 'medications': []},
+    {'illness': 'rabies', 'medications': ['rabies_vaccine', 'rabies_immunoglobulin']},
+    {'illness': 'canine_distemper', 'medications': ['doxycycline', 'amoxicillin', 'phenobarbital', 'potassium_bromide']},
+    {'illness': 'psittacosis', 'medications': ['doxycycline']}
+]
+
